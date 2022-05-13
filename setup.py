d1538544500d9e5ae8e103350359f9cf63187a7f
Glad B <gborn.new@gmail.com>
2022-05-13 16:56:03 +0530

    added setup and procfile to push to heroku

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"gborn.new@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
